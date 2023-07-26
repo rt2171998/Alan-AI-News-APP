54975bd8e3a92d098820b10695668010bcccbffe
rt2171998 <rt2171998@gmail.com>
2023-07-26 09:32:46 +0530

    final

---
+++ build/precache-manifest.c3f2550b2b953ddd76f492f1d58358a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41013125e04c1988ad301f3709908f77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce2160a9bddd71379f7d",
+    "url": "/static/css/main.c298bd42.chunk.css"
+  },
+  {
+    "revision": "78f7bacd4ad77c7cc761",
+    "url": "/static/js/2.dd8efa81.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dd8efa81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2160a9bddd71379f7d",
+    "url": "/static/js/main.d8aefc42.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from "react";
+import { Typography } from "@material-ui/core";
+import wordsToNumbers from "words-to-numbers";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+import logo from "./images/logo.png";
+import { NewsCards, Modal } from "./components";
+import useStyles from "./styles";
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: "3b34dc935a33e3f3a8f384584ae698452e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command, articles, number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "instructions") {
+          setIsOpen(true);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === "open") {
+          const parsedNumber =
+            number.length > 2
+              ? wordsToNumbers(number, { fuzzy: true })
+              : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText("Please try that again...");
+          } else if (article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText("Opening...");
+          } else {
+            alanBtn().playText("Please try that again...");
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles?.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Open article number [4]
+              </Typography>
+            </div>
+            <div className={classes.card}>
+              <Typography variant="h5" component="h2">
+                Try saying: <br />
+                <br />
+                Go back
+              </Typography>
+            </div>
+          </div>
+        ) : null}
+        <img
+          src="https://miro.medium.com/v2/resize:fit:1400/1*-DAj8QPi-tgAM52wKozk1A.jpeg"
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a
+              className={classes.link}
+              href="www.linkedin.com/in/rahul-tyagi-k24"
+              target="_blank"
+            >
+              {" "}
+              Rahul Tyagi-Linkedin
+            </a>
+          </Typography>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
